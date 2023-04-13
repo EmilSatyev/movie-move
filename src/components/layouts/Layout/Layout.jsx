7c57d94c2e02441f015bce83639ee722c9fed740
EmilSatyev <satyev.emil@gmail.com>
2023-04-13 11:21:17 +0500

    init

--- src/components/layouts/Layout/Layout.jsx
+++ src/components/layouts/Layout/Layout.jsx
@@ -2,14 +2,17 @@ import React, { useContext, useEffect, useState } from "react";
 import { Outlet, useLocation } from "react-router-dom";
 import styles from "./Layout.module.scss";
 import { Aside, ErrorBoundary, Header } from "../../common";
-import {ErrorFallback} from "../../common/ErrorBoundary/ErrorFallback";
+import { ErrorFallback } from "../../common/ErrorBoundary/ErrorFallback";
 
 const Layout = () => {
   const location = useLocation();
   const [isShowAside, setIsShowAside] = useState(true);
   useEffect(() => {
     window.scrollTo(0, 0);
-    setIsShowAside(!location.pathname.includes("actor"));
+    setIsShowAside(
+      !location.pathname.includes("actor") &&
+        !location.pathname.includes("search")
+    );
   }, [location]);
 
   return (
